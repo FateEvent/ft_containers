e44206cc906bb5560ecaef40a4d6cbb79194a83d
FateEvent <fabio.venturini.1989@gmail.com>
2022-11-30 16:18:39 +0100

    todo: clear

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -69,6 +69,13 @@ namespace ft
 				_alloc.construct(&*p, *first);
 		};
 
+		~vector()
+		{
+			for (iterator p = _v; p < _v + size(); ++p)
+				_alloc.destroy(&*p);
+			_alloc.deallocate(_v, _capacity);
+		}
+
 		vector&	operator= ( const vector& rhs )
 		{
 			if (this == rhs)
@@ -90,15 +97,8 @@ namespace ft
 			return (*this);
 		}
 
-		~vector()
-		{
-			for (iterator p = _v; p < _v + size(); ++p)
-				_alloc.destroy(&*p);
-			_alloc.deallocate(_v, _capacity);
-		}
-
 		void	assign( size_type count, const T& value ) {
-			for (iterator p = _v; p < _v + _size(); ++p)
+			for (iterator p = _v; p < _v + size(); ++p)
 				_alloc.destroy(&*p);
 			_alloc.deallocate(_v, _capacity);
 			_size = _capacity = count;
@@ -111,7 +111,7 @@ namespace ft
 		template< class InputIt >
 		void	assign( InputIt first, InputIt last )
 		{
-			for (iterator p = _v; p < _v + _size(); ++p)
+			for (iterator p = _v; p < _v + size(); ++p)
 				_alloc.destroy(&*p);
 			_alloc.deallocate(_v, _capacity);
 			ptrdiff_t	dist = last - first;
@@ -124,6 +124,21 @@ namespace ft
 
 		allocator_type	get_allocator() const { return _alloc; }
 
+		reference				at( size_type pos ) {
+			if (pos >= _size)
+				throw (ArrayException("out_of_range"));
+			return (&_v[pos]);
+		};
+		
+		const_reference			at( size_type pos ) const {
+			if (pos >= _size)
+				throw (ArrayException("out_of_range"));
+			return (&_v[pos]);
+		};
+
+		reference		operator[]( size_type pos ) { return &_v[pos]; };
+		const_reference	operator[]( size_type pos ) const { return &_v[pos]; }
+
 		reference front() { return (&begin()); }
 
 		const_reference front() const { return (&begin()); }
@@ -131,7 +146,28 @@ namespace ft
 		reference back() { return (&end() - 1); };
 
 		const_reference back() const { return (&end() - 1); };
+
+		value_type*	data() { return (size() ? _v : NULL); };
+
+		const value_type*	data() const { return (size() ? _v : NULL); };
+
+		iterator				begin()	{ return iterator(&_v[0]); }
+		const_iterator			begin() const	{ return const_iterator(&_v[0]); }
+		iterator				end()	{ return iterator(&_v[_size]); }
+		const_iterator			end() const	{ return const_iterator(&_v[_size]); }
+
+		reverse_iterator		rbegin()	{ return reverse_iterator(&_v[_size - 1]); }
+		const_reverse_iterator	rbegin() const	{ return const_reverse_iterator(&_v[_size - 1]); }
+		reverse_iterator		rend()	{ return reverse_iterator(&_v[-1]); }
+		const_reverse_iterator	rend() const	{ return const_reverse_iterator(&_v[-1]); }
 		
+		bool empty() const { return !size(); }
+
+		size_type	size() { return _size; }
+
+		size_type	max_size() { return std::min<size_type>(_alloc.max_size(),
+								std::numeric_limits<difference_type>::max()); }
+
 		void	reserve(size_type n)
 		{
 			if (n > capacity() && n < max_size())
@@ -147,6 +183,8 @@ namespace ft
 				_v = temp;
 			}
 		}
+
+		size_type	capacity() { return _capacity; }
 
 		void	push_back(value_type data)
 		{
@@ -196,61 +234,21 @@ namespace ft
 			}
 		}
 
-		size_type	size() { return _size; }
-
-		size_type	max_size() { return std::min<size_type>(_alloc.max_size(),
-								std::numeric_limits<difference_type>::max()); }
-
-		size_type	capacity() { return _capacity; }
-
-		void print()
-		{
-			for (size_type i = 0; i < size(); i++) {
-				std::cout << _v[i] << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		iterator				begin()	{ return iterator(&_v[0]); }
-		const_iterator			begin() const	{ return const_iterator(&_v[0]); }
-		iterator				end()	{ return iterator(&_v[_size]); }
-		const_iterator			end() const	{ return const_iterator(&_v[_size]); }
-
-		reverse_iterator		rbegin()	{ return reverse_iterator(&_v[_size - 1]); }
-		const_reverse_iterator	rbegin() const	{ return const_reverse_iterator(&_v[_size - 1]); }
-		reverse_iterator		rend()	{ return reverse_iterator(&_v[-1]); }
-		const_reverse_iterator	rend() const	{ return const_reverse_iterator(&_v[-1]); }
-
-		reference				operator[]( size_type pos ) { return &_v[pos]; };
-		const_reference			operator[]( size_type pos ) const { return &_v[pos]; }
-
-		reference				at( size_type pos ) {
-			if (pos >= _size)
-				throw (ArrayException("out_of_range"));
-			return (&_v[pos]);
-		};
-		
-		const_reference			at( size_type pos ) const {
-			if (pos >= _size)
-				throw (ArrayException("out_of_range"));
-			return (&_v[pos]);
-		};
-
-		value_type	&operator++ () { _v++; return *_v; }
-		value_type	operator++ (int) { value_type tmp = *_v; ++_v; return tmp; }
-		value_type	&operator-- () { _v--; return *_v; }
-		value_type	operator-- (int) { value_type tmp = *_v; --_v; return tmp; }		
-		value_type	&operator+= (std::size_t dist) { _v += dist; return *_v; }
-		value_type	&operator-= (std::size_t dist) { _v -= dist; return *_v; }
-		value_type	operator+ (std::size_t dist) const { return (_v + dist); }
-		value_type	operator- (std::size_t dist) const { return (_v - dist); }
-
 		bool		operator== (const vector &other) { return this->_v == other._v; }
 		bool		operator!= (const vector &other) { return this->_v != other._v; }
 		bool		operator< (const vector &other) { return this->_v < other._v; }
-		bool		operator> (const vector &other) { return this->_v > other._v; }
 		bool		operator<= (const vector &other) { return this->_v <= other._v; }
+		bool		operator> (const vector &other) { return this->_v > other._v; }
 		bool		operator>= (const vector &other) { return this->_v >= other._v; }
+
+//		value_type	&operator++ () { _v++; return *_v; }
+//		value_type	operator++ (int) { value_type tmp = *_v; ++_v; return tmp; }
+//		value_type	&operator-- () { _v--; return *_v; }
+//		value_type	operator-- (int) { value_type tmp = *_v; --_v; return tmp; }		
+//		value_type	&operator+= (std::size_t dist) { _v += dist; return *_v; }
+//		value_type	&operator-= (std::size_t dist) { _v -= dist; return *_v; }
+//		value_type	operator+ (std::size_t dist) const { return (_v + dist); }
+//		value_type	operator- (std::size_t dist) const { return (_v - dist); }
 	};
 }
 
