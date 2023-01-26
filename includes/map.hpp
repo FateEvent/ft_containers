7eadb28df1b6f32da444300a21d313d9192b7a38
FateEvent <fabio.venturini.1989@gmail.com>
2023-01-26 11:51:56 +0100

    wait for me

--- includes/map.hpp
+++ includes/map.hpp
@@ -20,23 +20,23 @@ namespace ft
 	class map {
 		struct Node;
 	public:
-		typedef Key																					key_type;
-		typedef T																					mapped_type;
-		typedef pair<const key_type, mapped_type>													value_type;
-		typedef Compare																				key_compare;
-		typedef Allocator																			allocator_type;
-		typedef typename allocator_type::reference													reference;
-		typedef typename allocator_type::const_reference											const_reference;
-		typedef typename allocator_type::pointer													pointer;
-		typedef typename allocator_type::const_pointer												const_pointer;
-		typedef typename allocator_type::size_type													size_type;
-		typedef typename allocator_type::difference_type											difference_type;
-		typedef typename Allocator:: template rebind<Node>::other									node_allocator;
-		typedef typename node_allocator::pointer													node_pointer;
-		typedef ft::wrapper_it<ft::map_iterator<key_type, mapped_type, Node, value_type> >			iterator;
-		typedef ft::wrapper_it<ft::map_iterator<key_type, mapped_type, Node, const value_type> >	const_iterator;
-		typedef ft::reverse_iterator<iterator>														reverse_iterator;
-		typedef ft::reverse_iterator<const_iterator>												const_reverse_iterator;
+		typedef Key															key_type;
+		typedef T															mapped_type;
+		typedef pair<const key_type, mapped_type>							value_type;
+		typedef Compare														key_compare;
+		typedef Allocator													allocator_type;
+		typedef typename allocator_type::reference							reference;
+		typedef typename allocator_type::const_reference					const_reference;
+		typedef typename allocator_type::pointer							pointer;
+		typedef typename allocator_type::const_pointer						const_pointer;
+		typedef typename allocator_type::size_type							size_type;
+		typedef typename allocator_type::difference_type					difference_type;
+		typedef typename Allocator:: template rebind<Node>::other			node_allocator;
+		typedef typename node_allocator::pointer							node_pointer;
+		typedef ft::wrapper_it<ft::map_iterator<value_type, Node> >			iterator;
+		typedef ft::wrapper_it<ft::map_iterator<const value_type, Node> >	const_iterator;
+		typedef ft::reverse_iterator<iterator>								reverse_iterator;
+		typedef ft::reverse_iterator<const_iterator>						const_reverse_iterator;
 
 		class value_compare
 			: public std::binary_function<value_type, value_type, bool>
--- includes/map_iterator.hpp
+++ includes/map_iterator.hpp
@@ -16,30 +16,28 @@ class reverse_iterator;
 
 namespace ft
 {
-	template <class Key, class T, class Node, class Content>
-	class map_iterator
+	template<class T, class Node>
+	class map_iterator : public iterator<std::bidirectional_iterator_tag, T>
 	{
 	public :
-		typedef	std::bidirectional_iterator_tag		iterator_category;
-		typedef Key									key_type;
-		typedef T									mapped_type;
-		typedef Content								value_type;
-		typedef std::size_t							size_type;
-		typedef std::ptrdiff_t						difference_type;
-		typedef Node*								node_pointer;
-		typedef Node&								node_reference;
-		typedef Content*							pointer;
-		typedef Content&							reference;
+		typedef	std::bidirectional_iterator_tag	iterator_category;
+		typedef T								value_type;
+		typedef std::size_t						size_type;
+		typedef std::ptrdiff_t					difference_type;
+		typedef Node*							node_pointer;
+		typedef Node&							node_reference;
+		typedef T*								pointer;
+		typedef T&								reference;
 
-		map_iterator() : _ptr(NULL), _last(NULL)  {}
-		map_iterator(node_pointer ptr) : _ptr(ptr), _last(ptr) {}
-		map_iterator(const map_iterator &other) : _ptr(other._ptr), _last(other._last) {}
+		map_iterator() : _ptr(NULL)  {}
+		map_iterator(node_pointer ptr) : _ptr(ptr) {}
+		map_iterator(const map_iterator &other) : _ptr(other._ptr) {}
 		template<typename U>
-		map_iterator(const map_iterator<Key, T, Node, U> &other,
+		map_iterator(const map_iterator<U, Node> &other,
 			typename ft::enable_if<!std::is_const<U>::value>::type* = 0)
-				: _ptr(other._ptr), _last(other._last) {}
-		map_iterator(const wrapper_it<map_iterator> &other) : _ptr(other.base()._ptr), _last(NULL) {}
-		map_iterator(const reverse_iterator<map_iterator> &other) : _ptr(other.base()._ptr), _last(NULL) {}
+				: _ptr(other._ptr){}
+		map_iterator(const wrapper_it<map_iterator> &other) : _ptr(other.base()._ptr) {}
+		map_iterator(const reverse_iterator<map_iterator> &other) : _ptr(other.base()._ptr) {}
 		~map_iterator() {}
 
 		map_iterator&	operator=(const map_iterator &assign) 
@@ -49,34 +47,19 @@ namespace ft
 			return (*this); 
 		}
 
-		pointer			base() const { return &(_ptr->data()); }
-		node_pointer	node_base() const { return _ptr; }
+		pointer		base() const { return (_ptr); }
 
-		reference	operator* () const { return _ptr->data(); }
-		pointer		operator-> () const { return &(_ptr->data()); }
+		reference	operator* () const { return (*_ptr); }
+		iterator	operator-> () const { return (_ptr); }
 		reference	operator[] (difference_type index) { return (base()[index]); }
 
-		operator	map_iterator<Key, T, Node, const value_type>() const { return (map_iterator<Key, T, Node, const value_type>(_ptr)); }
+		operator	map_iterator<const T, Node>() const { return (map_iterator<const T, Node>(_ptr)); }
 
 		void		set_ptr(node_pointer ptr) { _ptr = ptr; }
 
 		// pre-increment
 		map_iterator& operator++() {
-			if (_ptr && _ptr->_right) {
-				_ptr = _ptr->_right;
-				while (_ptr->_left)
-					_ptr = _ptr->_left;
-			} else {
-				Node	*tmp = _ptr;
-
-				_ptr = _ptr->_parent;
-				while (_ptr && tmp == _ptr->_right)
-				{
-					tmp = _ptr;
-					_ptr = _ptr->_parent;
-				};
-			}
-			return (*this);
+			_ptr = _ptr->next();
 		}
 
 		// post-increment
@@ -88,23 +71,7 @@ namespace ft
 
 		// pre-decrement
 		map_iterator& operator--() {
-			if (!_ptr)
-				_ptr = _last;
-			else if (_ptr && _ptr->_left) {
-				_ptr = _ptr->_left;
-				while (_ptr->_right)
-					_ptr = _ptr->_right;
-			} else {
-				Node	*tmp = _ptr;
-
-				_ptr = _ptr->_parent;
-				while (_ptr && tmp == _ptr->_left)
-				{
-					tmp = _ptr;
-					_ptr = _ptr->_parent;
-				};
-			}
-			return (*this);
+			_ptr = _ptr->prev();
 		}
 
 		// post-decrement
@@ -123,7 +90,7 @@ namespace ft
 		bool		operator== (const map_iterator &it) { return this->base() == it.base(); }
 
 		template<typename T1, typename T2>
-		friend bool	operator!= (const map_iterator<Key, T, Node, T1>& lhs, const map_iterator<Key, T, Node, T2>& rhs)
+		friend bool	operator!= (const map_iterator<T1, Node>& lhs, const map_iterator<T2, Node>& rhs)
 		{
 			return (lhs._ptr != rhs._ptr);
 		}
@@ -134,8 +101,95 @@ namespace ft
 		bool		operator>= (const map_iterator &it) { return this->base() >= it.base(); }
 
 	private:
-		Node	*_ptr;
-		Node	*_last;
+		pointer	*_ptr;
+	};
+
+	template<class T, class Node, class Iter>
+	class map_const_iterator : public iterator<std::bidirectional_iterator_tag, T>
+	{
+	public :
+		typedef	std::bidirectional_iterator_tag	iterator_category;
+		typedef T								value_type;
+		typedef std::size_t						size_type;
+		typedef std::ptrdiff_t					difference_type;
+		typedef Node*							node_pointer;
+		typedef Node&							node_reference;
+		typedef T*								pointer;
+		typedef T&								reference;
+
+		map_const_iterator() : _ptr(NULL)  {}
+		map_const_iterator(node_pointer ptr) : _ptr(ptr) {}
+		map_const_iterator(const map_const_iterator &other) : _ptr(other._ptr) {}
+		template<typename U>
+		map_const_iterator(const map_const_iterator<U, Node, Iter> &other,
+			typename ft::enable_if<!std::is_const<U>::value>::type* = 0)
+				: _ptr(other._ptr){}
+		map_const_iterator(const wrapper_it<map_const_iterator> &other) : _ptr(other.base()._ptr) {}
+		map_const_iterator(const reverse_iterator<map_const_iterator> &other) : _ptr(other.base()._ptr) {}
+		~map_const_iterator() {}
+
+		map_const_iterator&	operator=(const map_const_iterator &assign) 
+		{
+			if (this != &assign)
+				_ptr = assign._ptr;
+			return (*this); 
+		}
+
+		pointer		base() const { return (_ptr); }
+
+		reference	operator* () const { return (_ptr); }
+		iterator	operator-> () const { return (_ptr); }
+		reference	operator[] (difference_type index) { return (base()[index]); }
+
+		operator	map_const_iterator<T, Node, Iter>() const { return (map_const_iterator<const T, Node, Iter>(_ptr)); }
+
+		void		set_ptr(node_pointer ptr) { _ptr = ptr; }
+
+		// pre-increment
+		map_const_iterator& operator++() {
+			_ptr = _ptr->next();
+		}
+
+		// post-increment
+		map_const_iterator operator++(int) {
+			map_const_iterator old(*this);
+			operator++();
+			return (old);
+		}
+
+		// pre-decrement
+		map_const_iterator& operator--() {
+			_ptr = _ptr->prev();
+		}
+
+		// post-decrement
+		map_const_iterator operator--(int) {
+			map_const_iterator old(*this);
+			operator--();
+			return (old);
+		}
+
+		const map_const_iterator	operator+ (const difference_type dist) const { return (_ptr + dist); }
+		const map_const_iterator	&operator+= (const difference_type dist) { _ptr += dist; return (*this); }
+		const map_const_iterator	operator- (const difference_type dist) const { return (_ptr - dist); }
+		const map_const_iterator	&operator-= (const difference_type dist) { _ptr -= dist; return (*this); }
+		ptrdiff_t					operator- (const map_const_iterator &it) { return (_ptr - it._ptr); }
+
+		bool	operator== (const map_const_iterator &it) { return this->base() == it.base(); }
+
+		template<typename T1, typename T2>
+		friend bool	operator!= (const map_const_iterator<T1, Node>& lhs, const map_const_iterator<T2, Node>& rhs)
+		{
+			return (lhs._ptr != rhs._ptr);
+		}
+
+		bool	operator< (const map_const_iterator &it) { return this->base() < it.base(); }
+		bool	operator<= (const map_const_iterator &it) { return this->base() <= it.base(); }
+		bool	operator> (const map_const_iterator &it) { return this->base() > it.base(); }
+		bool	operator>= (const map_const_iterator &it) { return this->base() >= it.base(); }
+
+	private:
+		pointer	*_ptr;
 	};
 }
 
